7c7b24cc30693d94993fab06ae58b02ac2d41de0
cor03rock <rocky.coronel@globalzeal.net>
2014-04-29 15:59:51 +0800

    added jquery.min.js and added js for next functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-script.js
+$(document).ready(function() {
+	
+    $( '#previous' ).click(function( e ){
+    	e.preventDefault();
+    	console.log("gwapo ko");
+});
+
+      $( '#next' ).click(function( e ){
+    	e.preventDefault();
+    	console.log("gwapo cya");
+    	console.log($( '.activeSlide' ).next());
+    	if($( '.activeSlide' ).next().length === 0 ){
+    		//check if there is next element, if length = 0, back to 1st <li> 
+    		$('.slideImg > li:first-child').addClass( 'activeSlide' ).removeClass('inactiveSlide').show();
+    		$( '.slideImg > li:last-child' ).removeClass( 'activeSlide' ).addClass("inactiveSlide").hide();
+    	}else {
+    		$( '.activeSlide' ).next().addClass( 'activeSlide' ).removeClass("inactiveSlide").show();
+    		$( '.activeSlide' ).prev().removeClass( 'activeSlide' ).addClass("inactiveSlide").hide();
+    	}
+    });
+});
+
+
+	
+  
